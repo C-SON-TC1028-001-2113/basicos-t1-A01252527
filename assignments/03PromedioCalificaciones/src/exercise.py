fb89fbf178b58d2fb19313df5124a866c9f547e7
A01252527 <a01252527@itesm.mx>
2021-08-17 03:24:42 +0000

    tarea 1 ejercicio 2

---
+++ assignments/03PromedioCalificaciones/src/exercise.py
@@ -0,0 +1,8 @@
+def main():
+    #escribe tu código abajo de esta línea
+    a = int(input("Cual es el primer numero? "))
+    b = int(input("Cual es el segundo numero? "))
+    c= a + b
+    print (c)
+if __name__ == '__main__':
+    main()
